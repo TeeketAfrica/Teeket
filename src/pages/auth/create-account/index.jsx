f12670f6fa13e3b1b55e1248b28a2a015623b935
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2023-12-28 23:07:30 +0100

    update: convert orm functionalities to component

--- src/pages/auth/create-account/index.jsx
+++ src/pages/auth/create-account/index.jsx
@@ -1,71 +1,12 @@
-import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { Image } from '@chakra-ui/image';
-import { Stack, Box, Divider, Text } from '@chakra-ui/layout';
-import {
-  FormErrorMessage,
-  FormLabel,
-  FormControl,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Checkbox,
-  Button,
-} from '@chakra-ui/react';
+import { Box, Divider, Text } from '@chakra-ui/layout';
 
 import AuthLayout from '../../../components/auth/AuthLayout';
 import AuthHeader from '../../../components/auth/AuthHeader';
 import GoogleBtn from '../../../components/auth/GoogleBtn';
-
-import MailIcon from '../../../assets/icon/MailIcon.svg';
-import CloseIcon from '../../../assets/icon/CloseIcon.svg';
-import EyeSlashIcon from '../../../assets/icon/EyeSlashIcon.svg';
+import CreateAccountForm from './components/CreateAccountForm';
 
 const CreateAccountPage = () => {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { errors },
-  } = useForm();
-
-  const password = watch('password');
-
-  const [passwordCriteria, setPasswordCriteria] = useState({
-    hasUppercase: false,
-    hasLowercase: false,
-    hasMinLength: false,
-    hasSpecialChar: false,
-  });
-
-  useEffect(() => {
-    if (password) {
-      setPasswordCriteria({
-        hasUppercase: /[A-Z]/.test(password),
-        hasLowercase: /[a-z]/.test(password),
-        hasMinLength: password.length >= 8,
-        hasSpecialChar: /[!@#$%^&*(),.?":{}|<>]/.test(password),
-      });
-    } else {
-      // Reset criteria if password is empty
-      setPasswordCriteria({
-        hasUppercase: false,
-        hasLowercase: false,
-        hasMinLength: false,
-        hasSpecialChar: false,
-      });
-    }
-  }, [password]);
-
-  const [viewPassword, setViewPassword] = useState(false);
-  const [formError] = useState({
-    invalidEmail: false,
-    incorrectPassword: false,
-  });
-
-  const onSubmit = (data) => console.log(data);
-
   return (
     <AuthLayout>
       <Box display="flex" flexDirection="column" gap={8}>
@@ -83,126 +24,7 @@ const CreateAccountPage = () => {
           </Box>
         </Box>
         <Box>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Stack spacing={4}>
-              <FormControl isInvalid={errors.email || formError.invalidEmail}>
-                <FormLabel
-                  htmlFor="email"
-                  fontSize="sm"
-                  fontWeight="medium"
-                  color="gray.800"
-                >
-                  Email Address
-                </FormLabel>
-                <InputGroup size="lg">
-                  <InputRightElement pointerEvents="none">
-                    {formError.invalidEmail || errors.email ? (
-                      <Image src={CloseIcon} alt="close icon" />
-                    ) : (
-                      <Image src={MailIcon} alt="mail icon" />
-                    )}
-                  </InputRightElement>
-                  <Input
-                    id="email"
-                    type="text"
-                    {...register('email', {
-                      required: 'This is required',
-                      pattern: {
-                        value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                        message: 'Invalid email address',
-                      },
-                    })}
-                  />
-                </InputGroup>
-                <FormErrorMessage color="red.500">
-                  {errors.email && errors.email.message}
-                  {formError.invalidEmail && !errors.email && (
-                    <Text as="span">This email account already exists</Text>
-                  )}
-                </FormErrorMessage>
-              </FormControl>
-              <FormControl
-                isInvalid={errors.password || formError.incorrectPassword}
-              >
-                <FormLabel
-                  htmlFor="password"
-                  fontSize="sm"
-                  fontWeight="medium"
-                  color="gray.800"
-                >
-                  Password
-                </FormLabel>
-                <InputGroup size="lg">
-                  <InputRightElement
-                    cursor="pointer"
-                    onClick={() => setViewPassword((prev) => !prev)}
-                  >
-                    {formError.incorrectPassword || errors.password ? (
-                      <Image src={CloseIcon} alt="close icon" />
-                    ) : (
-                      <Image src={EyeSlashIcon} alt="eye icon" />
-                    )}
-                  </InputRightElement>
-                  <Input
-                    id="password"
-                    type={`${viewPassword ? 'text' : 'password'}`}
-                    {...register('password', {
-                      required: 'This is required',
-                      pattern: {
-                        value:
-                          /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*(),.?":{}|<>]).{8,}$/i,
-                        message:
-                          'You have not met all the password requirments',
-                      },
-                    })}
-                  />
-                </InputGroup>
-                <FormErrorMessage color="red.500">
-                  {errors.password && errors.password.message}
-                  {formError.incorrectPassword && !errors.password && (
-                    <Text as="span">Incorrect password. Please try again</Text>
-                  )}
-                </FormErrorMessage>
-                <Box mt="4">
-                  <Stack
-                    spacing={2}
-                    direction="row"
-                    flexWrap="wrap"
-                    pointerEvents="none"
-                  >
-                    <Checkbox
-                      id="hasUppercase"
-                      name="hasUppercase"
-                      isChecked={passwordCriteria?.hasUppercase}
-                    >
-                      Uppercase
-                    </Checkbox>
-                    <Checkbox
-                      id="hasLowercase"
-                      name="hasLowercase"
-                      isChecked={passwordCriteria?.hasLowercase}
-                    >
-                      Lowercase
-                    </Checkbox>
-                    <Checkbox
-                      id="hasMinLength"
-                      name="hasMinLength"
-                      isChecked={passwordCriteria?.hasMinLength}
-                    >
-                      More than 8 characters
-                    </Checkbox>
-                    <Checkbox
-                      name="hasSpecialChar"
-                      isChecked={passwordCriteria?.hasSpecialChar}
-                    >{`Special character(e.g. /,<>@#$%)`}</Checkbox>
-                  </Stack>
-                </Box>
-              </FormControl>
-              <Button type="submit" mt="4" size="lg">
-                Login
-              </Button>
-            </Stack>
-          </form>
+          <CreateAccountForm />
         </Box>
         <Box>
           <Text color="gray.600" textAlign="center" fontSize="sm">
--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,42 +1,15 @@
-import { useState } from 'react';
 import { Link } from 'react-router-dom';
-import { useForm } from 'react-hook-form';
-import { Image } from '@chakra-ui/image';
-import { Stack, Box, Divider, Text } from '@chakra-ui/layout';
+import { Box, Divider, Text } from '@chakra-ui/layout';
 import { useTheme } from '@chakra-ui/system';
-import {
-  FormErrorMessage,
-  FormLabel,
-  FormControl,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Button,
-} from '@chakra-ui/react';
+
 import AuthLayout from '../../../components/auth/AuthLayout';
 import AuthHeader from '../../../components/auth/AuthHeader';
 import GoogleBtn from '../../../components/auth/GoogleBtn';
 
-import MailIcon from '../../../assets/icon/MailIcon.svg';
-import CloseIcon from '../../../assets/icon/CloseIcon.svg';
-import EyeSlashIcon from '../../../assets/icon/EyeSlashIcon.svg';
+import LoginForm from './components/LoginForm';
 
 const LoginPage = () => {
   const theme = useTheme();
-  const {
-    register,
-    handleSubmit,
-    // watch,
-    formState: { errors },
-  } = useForm();
-
-  const [viewPassword, setViewPassword] = useState(false);
-  const [formError] = useState({
-    invalidEmail: false,
-    incorrectPassword: false,
-  });
-
-  const onSubmit = (data) => console.log(data);
 
   return (
     <div>
@@ -56,88 +29,7 @@ const LoginPage = () => {
             </Box>
           </Box>
           <Box>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Stack spacing={4}>
-                <FormControl isInvalid={errors.email || formError.invalidEmail}>
-                  <FormLabel
-                    htmlFor="email"
-                    fontSize="sm"
-                    fontWeight="medium"
-                    color="gray.800"
-                  >
-                    Email Address
-                  </FormLabel>
-                  <InputGroup size="lg">
-                    <InputRightElement pointerEvents="none">
-                      {formError.invalidEmail || errors.email ? (
-                        <Image src={CloseIcon} alt="close icon" />
-                      ) : (
-                        <Image src={MailIcon} alt="mail icon" />
-                      )}
-                    </InputRightElement>
-                    <Input
-                      id="email"
-                      type="text"
-                      {...register('email', {
-                        required: 'This is required',
-                        pattern: {
-                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                          message: 'Invalid email address',
-                        },
-                      })}
-                    />
-                  </InputGroup>
-                  <FormErrorMessage color="red.500">
-                    {errors.email && errors.email.message}
-                    {formError.invalidEmail && !errors.email && (
-                      <Text as="span">Not a valid email address</Text>
-                    )}
-                  </FormErrorMessage>
-                </FormControl>
-                <FormControl
-                  isInvalid={errors.password || formError.incorrectPassword}
-                >
-                  <FormLabel
-                    htmlFor="password"
-                    fontSize="sm"
-                    fontWeight="medium"
-                    color="gray.800"
-                  >
-                    Password
-                  </FormLabel>
-                  <InputGroup size="lg">
-                    <InputRightElement
-                      cursor="pointer"
-                      onClick={() => setViewPassword((prev) => !prev)}
-                    >
-                      {formError.incorrectPassword || errors.password ? (
-                        <Image src={CloseIcon} alt="close icon" />
-                      ) : (
-                        <Image src={EyeSlashIcon} alt="eye icon" />
-                      )}
-                    </InputRightElement>
-                    <Input
-                      id="password"
-                      type={`${viewPassword ? 'text' : 'password'}`}
-                      {...register('password', {
-                        required: 'This is required',
-                      })}
-                    />
-                  </InputGroup>
-                  <FormErrorMessage color="red.500">
-                    {errors.password && errors.password.message}
-                    {formError.incorrectPassword && !errors.password && (
-                      <Text as="span">
-                        Incorrect password. Please try again
-                      </Text>
-                    )}
-                  </FormErrorMessage>
-                </FormControl>
-                <Button type="submit" mt="4" size="lg">
-                  Submit
-                </Button>
-              </Stack>
-            </form>
+            <LoginForm />
           </Box>
           <Box>
             <Text
