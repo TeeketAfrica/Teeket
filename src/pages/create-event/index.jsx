ecd6b25d2180292662c40ec52cc7795d073214bc
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2024-01-14 20:43:42 +0100

    merge changes

--- src/pages/create-event/index.jsx
+++ src/pages/create-event/index.jsx
@@ -1,45 +1,20 @@
-import { useState } from "react";
+import { useState } from 'react';
 
-<<<<<<< HEAD
 import Layout from './components/Layout';
 import FormStep1 from './layout/FormStep1';
 import FormStep2 from './layout/FormStep2';
 import FormStep3 from './layout/FormStep3';
-import FormStep4 from './layout/FormStep4';
-=======
-import { useForm, FormProvider } from "react-hook-form";
-
-import Layout from "./components/Layout";
-import FormStep1 from "./layout/FormStep1";
-import FormStep2 from "./layout/FormStep2";
-import FormStep3 from "./layout/FormStep3";
-import PublishEvent from "./layout/PublishEvent";
->>>>>>> dev
+import PublishEvent from './layout/PublishEvent';
 
 const VendorPage = () => {
   const [activeStep, setActiveStep] = useState(0);
 
   const handleNextStep = async () => {
-    // await handleSubmit((data) => {
-    //   Object.keys(data).forEach((fieldName) => {
-    //     setValue(fieldName, data[fieldName]);
-    //   });
-    // })();
-
     setActiveStep((prevStep) => prevStep + 1);
   };
 
   const handlePrevStep = () => {
     setActiveStep((prevStep) => prevStep - 1);
-<<<<<<< HEAD
-=======
-    reset();
-  };
-
-  const onSubmit = (data) => {
-    console.log("Final form data:", data);
-    // Add logic for submitting data to the server or performing other finalization steps
->>>>>>> dev
   };
 
   const renderFormSteps = () => {
