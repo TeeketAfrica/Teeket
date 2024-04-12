ad64aac7e3c3cf5af6a8f3648d4a342fc8acec45
Adekeye Emmanuel <adekeyeimmanuel@gmail.com>
2024-04-12 11:45:33 +0100

    chore - EventDetails component into smaller sub-components

--- src/pages/events/EventBookingPage.jsx
+++ src/pages/events/EventBookingPage.jsx
@@ -1,21 +1,18 @@
-import { useState } from "react";
 import { Button, Divider, Flex, Text, VStack } from "@chakra-ui/react";
 
 import Header from "../../components/layouts/Header";
 import Container from "../../components/ui/Container";
 import Footer from "../../components/layouts/Footer";
 
-import EventBookingDetail from "./components/EventBookingDetail";
+import EventBookingDetail from "./components/event-booking/EventBookingDetail";
 import FreeEvents from "./components/FreeEvents";
 
 const EventBooking = () => {
-  const [isRegistered] = useState(false);
-
   return (
     <main>
       <Header userInfo={true} />
       <Container padding="16px">
-        <EventBookingDetail isRegistered={isRegistered} />
+        <EventBookingDetail />
         <Divider borderColor="gray.300" borderWidth="1px" />
         <VStack paddingTop="11">
           <Flex
