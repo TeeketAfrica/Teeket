fa61b3173d84b83a9c23910356a4de6f7724fe72
Isaac Idibia <idibiaisaac3@gmail.com>
2024-05-27 13:51:06 +0100

    feat/auth-changes

--- src/routes/authenticatedRoutes.jsx
+++ src/routes/authenticatedRoutes.jsx
@@ -13,37 +13,42 @@ import {
   NotFoundPage,
 } from "../pages";
 import RouteGuard from "./routeGuard";
-import { useSelector } from "react-redux";
-import { selectUserDetails } from "../features/userSlice";
+// import { useSelector } from "react-redux";
+// import { selectUserDetails } from "../features/userSlice";
+import useIsUserAuthenticated from "./useIsUserAuthenticated";
 
-const isUserAuthenticated = () => {
-  const { token } = useSelector(selectUserDetails);
+// const isUserAuthenticated = () => {
+//   const { token } = useSelector(selectUserDetails);
 
-  if (token) {
-    return true;
-  }
+//   if (token) {
+//     return true;
+//   }
 
-  return false;
+//   return false;
+// };
+
+const AuthenticatedRoutes = () => {
+  const isAuthenticated = useIsUserAuthenticated();
+
+  return (
+    <RouteGuard isAllowed={isAuthenticated}>
+      <Routes>
+        <Route path="overview" element={<OverviewDashboardPage />} />
+        <Route path="events" element={<EventsDashboardPage />} />
+        <Route path="order" element={<OrdersDashboardPage />} />
+        <Route path="finance" element={<FinancesDashboardPage />} />
+        <Route
+          path="organization-settings"
+          element={<OrganizationSettingsDashboardPage />}
+        />
+        <Route path="events" element={<EventsPage />} />
+        <Route path="event-category" element={<EventCategoryPage />} />
+        <Route path="event-booking" element={<EventBookingPage />} />
+        <Route path="my-tickets" element={<TicketDashboardPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </RouteGuard>
+  );
 };
-
-const AuthenticatedRoutes = () => (
-  <RouteGuard isAllowed={isUserAuthenticated()}>
-    <Routes>
-      <Route path="overview" element={<OverviewDashboardPage />} />
-      <Route path="events" element={<EventsDashboardPage />} />
-      <Route path="order" element={<OrdersDashboardPage />} />
-      <Route path="finance" element={<FinancesDashboardPage />} />
-      <Route
-        path="organization-settings"
-        element={<OrganizationSettingsDashboardPage />}
-      />
-      <Route path="events" element={<EventsPage />} />
-      <Route path="event-category" element={<EventCategoryPage />} />
-      <Route path="event-booking" element={<EventBookingPage />} />
-      <Route path="my-tickets" element={<TicketDashboardPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
-  </RouteGuard>
-);
 
 export default AuthenticatedRoutes;
