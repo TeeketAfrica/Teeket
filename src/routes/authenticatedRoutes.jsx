db220c6cb04d0a37be5d3a81bc6eecad0f8dba53
Isaac Idibia <idibiaisaac3@gmail.com>
2024-05-28 18:56:16 +0100

    feat/merge-from-dev

--- src/routes/authenticatedRoutes.jsx
+++ src/routes/authenticatedRoutes.jsx
@@ -14,46 +14,6 @@ import {
   VendorPage,
 } from "../pages";
 import RouteGuard from "./routeGuard";
-<<<<<<< HEAD
-// import { useSelector } from "react-redux";
-// import { selectUserDetails } from "../features/userSlice";
-import useIsUserAuthenticated from "./useIsUserAuthenticated";
-
-// const isUserAuthenticated = () => {
-//   const { token } = useSelector(selectUserDetails);
-
-//   if (token) {
-//     return true;
-//   }
-
-//   return false;
-// };
-
-const AuthenticatedRoutes = () => {
-  const isAuthenticated = useIsUserAuthenticated();
-
-  return (
-    <RouteGuard isAllowed={isAuthenticated}>
-      <Routes>
-        <Route path="overview" element={<OverviewDashboardPage />} />
-        <Route path="events" element={<EventsDashboardPage />} />
-        <Route path="order" element={<OrdersDashboardPage />} />
-        <Route path="finance" element={<FinancesDashboardPage />} />
-        <Route
-          path="organization-settings"
-          element={<OrganizationSettingsDashboardPage />}
-        />
-        <Route path="events" element={<EventsPage />} />
-        <Route path="event-category" element={<EventCategoryPage />} />
-        <Route path="event-booking" element={<EventBookingPage />} />
-        <Route path="my-tickets" element={<TicketDashboardPage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
-    </RouteGuard>
-  );
-};
-
-=======
 import useIsUserAuth from "../hooks/useIsUserAuth";
 
 const AuthenticatedRoutes = () => {
@@ -81,5 +41,4 @@ const AuthenticatedRoutes = () => {
   );
 };
 
->>>>>>> dev
 export default AuthenticatedRoutes;
