bfa68702401a934b92e8e9ac5947dd845bf9f3f2
kemisolaola <kemisola077@gmail.com>
2025-01-18 12:31:25 +0100

    feat: fix issues raised on event and google auth

--- src/pages/auth/login/components/LoginForm.jsx
+++ src/pages/auth/login/components/LoginForm.jsx
@@ -13,116 +13,116 @@ import { useStorage } from "../../../../utils/storage";
 import { selectActiveUser } from "../../../../features/activeUserSlice";
 
 const LoginForm = () => {
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
-  const activeUser = useSelector(selectActiveUser);
-  const { setAccessToken, setRefreshToken } = useStorage();
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const activeUser = useSelector(selectActiveUser);
+    const { setAccessToken, setRefreshToken } = useStorage();
 
-  // Validation schema using Yup
-  const validationSchema = Yup.object({
-    email: Yup.string()
-      .email("Invalid email address")
-      .required("Please input your email address"),
-    password: Yup.string().required("Please input your password"),
-  });
+    // Validation schema using Yup
+    const validationSchema = Yup.object({
+        email: Yup.string()
+            .email("Invalid email address")
+            .required("Please input your email address"),
+        password: Yup.string().required("Please input your password"),
+    });
 
-  const [error, setError] = useState("");
+    const [error, setError] = useState("");
 
-  // Formik initialization
-  const formik = useFormik({
-    initialValues: {
-      email: "",
-      password: "",
-    },
-    validationSchema,
-    onSubmit: async (values) => {
-      try {
-        const response = await authApi.post("/login", {
-          email: values.email,
-          password: values.password,
-        });
+    // Formik initialization
+    const formik = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema,
+        onSubmit: async (values) => {
+            try {
+                const response = await authApi.post("/login", {
+                    email: values.email,
+                    password: values.password,
+                });
 
-        const access_token = response.data.access_token;
-        const refresh_token = response.data.refresh_token;
+                const access_token = response.data.access_token;
+                const refresh_token = response.data.refresh_token;
 
-        console.log("Login successful:", response);
+                console.log("Login successful:", response);
 
-        if (access_token) {
-          // set the refresh token and access token to cookie storage
-          setAccessToken(access_token);
-          setRefreshToken(refresh_token);
+                if (access_token) {
+                    // set the refresh token and access token to cookie storage
+                    setAccessToken(access_token);
+                    setRefreshToken(refresh_token);
 
-          dispatch(setUserDetails(values));
+                    dispatch(setUserDetails(values));
 
-          const path = sessionStorage.getItem("REDIRECT");
-          if (path) {
-            sessionStorage.removeItem("REDIRECT");
-            navigate(path);
-          }
+                    const path = sessionStorage.getItem("REDIRECT");
+                    if (path) {
+                        sessionStorage.removeItem("REDIRECT");
+                        navigate(path);
+                    }
 
-          if (!activeUser?.is_creator) {
-            navigate("/events");
-          } else {
-            navigate("/app/overview");
-          }
-        }
-      } catch (err) {
-        if (err.message == "Network Error") {
-          alert("Check your internet connection!.");
-        } else {
-          setError("Invalid username or password");
-        }
-        console.log("Failed to login", err.message);
-      }
-    },
-  });
+                    if (!activeUser?.is_creator) {
+                        navigate("/events");
+                    } else {
+                        navigate("/app/overview");
+                    }
+                }
+            } catch (err) {
+                if (err.message == "Network Error") {
+                    alert("Check your internet connection!.");
+                } else {
+                    setError("Invalid username or password");
+                }
+                console.log("Failed to login", err.message);
+            }
+        },
+    });
 
-  return (
-    <form onSubmit={formik.handleSubmit}>
-      <Stack spacing={4}>
-        {/* Email Address */}
-        <EmailInput
-          formik={{
-            handleChange: formik.handleChange,
-            values: formik.values,
-            touched: formik.touched,
-            errors: formik.errors,
-            setFieldTouched: formik.setFieldTouched,
-          }}
-          label="Email address"
-          inputName="email"
-          error={error}
-          handleError={setError}
-        />
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <Stack spacing={4}>
+                {/* Email Address */}
+                <EmailInput
+                    formik={{
+                        handleChange: formik.handleChange,
+                        values: formik.values,
+                        touched: formik.touched,
+                        errors: formik.errors,
+                        setFieldTouched: formik.setFieldTouched,
+                    }}
+                    label="Email address"
+                    inputName="email"
+                    error={error}
+                    handleError={setError}
+                />
 
-        {/* Password */}
-        <PasswordInput
-          formik={{
-            handleChange: formik.handleChange,
-            values: formik.values,
-            touched: formik.touched,
-            errors: formik.errors,
-            setFieldTouched: formik.setFieldTouched,
-          }}
-          label="Password"
-          inputName="password"
-          error={error}
-          handleError={setError}
-        />
+                {/* Password */}
+                <PasswordInput
+                    formik={{
+                        handleChange: formik.handleChange,
+                        values: formik.values,
+                        touched: formik.touched,
+                        errors: formik.errors,
+                        setFieldTouched: formik.setFieldTouched,
+                    }}
+                    label="Password"
+                    inputName="password"
+                    error={error}
+                    handleError={setError}
+                />
 
-        {/* Submit button */}
-        <Button
-          type="submit"
-          mt="4"
-          size="lg"
-          variant="primary"
-          isDisabled={formik.isSubmitting}
-        >
-          Login
-        </Button>
-      </Stack>
-    </form>
-  );
+                {/* Submit button */}
+                <Button
+                    type="submit"
+                    mt="4"
+                    size="lg"
+                    variant="primary"
+                    isDisabled={formik.isSubmitting}
+                >
+                    Login
+                </Button>
+            </Stack>
+        </form>
+    );
 };
 
 export default LoginForm;
