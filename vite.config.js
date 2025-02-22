2e93f155cf74a9ba48a7adc11f1572a1951cb694
Ogbonnaya Peter <ogbonnapete210@gmail.com>
2025-02-22 15:42:27 +0100

    add: import alias

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 import svgr from "vite-plugin-svgr";
 
 // https://vitejs.dev/config/
@@ -17,4 +18,9 @@ export default defineConfig({
       include: "**/*.svg",
     }),
   ],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
 });
