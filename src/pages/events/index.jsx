a4ce2aafba4786a39c7531d39c6435ddd678e357
Isaac Idibia <idibiaisaac3@gmail.com>
2025-05-05 09:43:56 +0100

    refactor: clean up formatting and improve readability in EventsPage component

--- src/pages/events/index.jsx
+++ src/pages/events/index.jsx
@@ -15,115 +15,107 @@ import { useStorage } from "../../utils/storage";
 import { teeketApi } from "../../utils/api";
 
 const EventsPage = () => {
-    const navigate = useNavigate();
-    const { getAccessToken } = useStorage();
+  const navigate = useNavigate();
+  const { getAccessToken } = useStorage();
 
-    const token = getAccessToken();
-    const { onOpen, isOpen, onClose } = useDisclosure();
-    const { searchTerm, category, clearSearch } = useContext(SearchContext);
+  const token = getAccessToken();
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  const { searchTerm, category, clearSearch } = useContext(SearchContext);
 
-    const [events, setEvents] = useState([]);
-    const [displayEventPreference, setDisplayEventPreference] = useState(false);
-    const [preloader, setPreLoader] = useState(true);
-    const [fetchError, setFetchError] = useState(false);
+  const [events, setEvents] = useState([]);
+  const [displayEventPreference, setDisplayEventPreference] = useState(false);
+  const [preloader, setPreLoader] = useState(true);
+  const [fetchError, setFetchError] = useState(false);
 
-    useEffect(() => {
-        fetchEvents(searchTerm, category);
-    }, [searchTerm, category]);
+  useEffect(() => {
+    fetchEvents(searchTerm, category);
+  }, [searchTerm, category]);
 
-    useEffect(() => {
-        if (!token && !displayEventPreference) {
-            setDisplayEventPreference(true);
-            onOpen();
-        }
-    }, [token, displayEventPreference, onOpen]);
+  useEffect(() => {
+    // if (!token && !displayEventPreference) {
+    setDisplayEventPreference(true);
+    onOpen();
+    // }
+    // }, [token, displayEventPreference, onOpen]);
+  }, [onOpen]);
 
-    const fetchEvents = async (title) => {
-        try {
-            const response = await teeketApi.get(`/events?title=${title}`, {
-                nullAuth: true,
-            });
-            const eventList = response.data.data;
+  const fetchEvents = async (title) => {
+    try {
+      const response = await teeketApi.get(`/events?title=${title}`, {
+        nullAuth: true,
+      });
+      const eventList = response.data.data;
 
-            setEvents(eventList);
+      setEvents(eventList);
 
-            setPreLoader(false);
-            setFetchError(false);
-        } catch (error) {
-            setPreLoader(false);
-            setFetchError(true);
-            console.error("Error fetching events:", error);
-        }
-    };
+      setPreLoader(false);
+      setFetchError(false);
+    } catch (error) {
+      setPreLoader(false);
+      setFetchError(true);
+      console.error("Error fetching events:", error);
+    }
+  };
 
-    return (
-        <main>
-            <ScrollToTop />
-            <Header />
-            {displayEventPreference && (
-                <EventPreference isOpen={isOpen} onClose={onClose} />
-            )}
-            <HeroSection />
-            {preloader ? (
-                <Text padding="40px 0" textAlign="center" fontSize="20px">
-                    {" "}
-                    Loading ...{" "}
-                </Text>
-            ) : (
-                <>
-                    {events.length > 0 && <EventTabs allEvents={events} />}
+  return (
+    <main>
+      <ScrollToTop />
+      <Header />
+      {displayEventPreference && (
+        <EventPreference isOpen={isOpen} onClose={onClose} />
+      )}
+      <HeroSection />
+      {preloader ? (
+        <Text padding="40px 0" textAlign="center" fontSize="20px">
+          {" "}
+          Loading ...{" "}
+        </Text>
+      ) : (
+        <>
+          {events.length > 0 && <EventTabs allEvents={events} />}
 
-                    {events.length == 0 && fetchError && (
-                        <Container maxW="385px" px={0}>
-                            <EmptyState
-                                icon={EventCautionState}
-                                title="Something went wrong"
-                                maxW="350px"
-                                desc={
-                                    <Text
-                                        fontSize={14}
-                                        color="gray.600"
-                                        textAlign="center"
-                                    >
-                                        We had some trouble loading this page.
-                                        Please refresh the page to try again
-                                    </Text>
-                                }
-                                outlineBtn="Contact support"
-                                primaryBtn="Refresh page"
-                                outlineOnClick={() => navigate("/support")}
-                                primaryOnClick={() => window.location.reload()}
-                            />
-                        </Container>
-                    )}
+          {events.length == 0 && fetchError && (
+            <Container maxW="385px" px={0}>
+              <EmptyState
+                icon={EventCautionState}
+                title="Something went wrong"
+                maxW="350px"
+                desc={
+                  <Text fontSize={14} color="gray.600" textAlign="center">
+                    We had some trouble loading this page. Please refresh the
+                    page to try again
+                  </Text>
+                }
+                outlineBtn="Contact support"
+                primaryBtn="Refresh page"
+                outlineOnClick={() => navigate("/support")}
+                primaryOnClick={() => window.location.reload()}
+              />
+            </Container>
+          )}
 
-                    {events.length === 0 && (
-                        <Container maxW="385px" px={0}>
-                            <EmptyState
-                                icon={EventSpeakerEmpty}
-                                title="No event found"
-                                desc={
-                                    <Text
-                                        fontSize={14}
-                                        color="gray.600"
-                                        textAlign="center"
-                                    >
-                                        “{searchTerm}” did not match any
-                                        results. please try again
-                                    </Text>
-                                }
-                                outlineBtn="Clear search"
-                                outlineOnClick={() => clearSearch()}
-                                primaryBtn="Create an event"
-                                primaryOnClick={() => navigate("/create-event")}
-                            />
-                        </Container>
-                    )}
-                </>
-            )}
-            <Footer />
-        </main>
-    );
+          {events.length === 0 && (
+            <Container maxW="385px" px={0}>
+              <EmptyState
+                icon={EventSpeakerEmpty}
+                title="No event found"
+                desc={
+                  <Text fontSize={14} color="gray.600" textAlign="center">
+                    “{searchTerm}” did not match any results. please try again
+                  </Text>
+                }
+                outlineBtn="Clear search"
+                outlineOnClick={() => clearSearch()}
+                primaryBtn="Create an event"
+                primaryOnClick={() => navigate("/create-event")}
+              />
+            </Container>
+          )}
+        </>
+      )}
+      <Footer />
+    </main>
+  );
 };
 
 export default EventsPage;
